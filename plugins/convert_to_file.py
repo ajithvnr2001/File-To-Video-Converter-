60fe488530b839f06a6b4f873c19b633d74d7029
ajithvnr2001 <74783525+ajithvnr2001@users.noreply.github.com>
2022-12-23 12:14:25 +0530

    Update convert_to_file.py

--- plugins/convert_to_file.py
+++ plugins/convert_to_file.py
@@ -28,6 +28,8 @@ from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 # https://stackoverflow.com/a/37631799/4723940
 from PIL import Image
+from pyrogram import filters
+from pyrogram import InlineKeyboardMarkup
 
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["c2f"]))
